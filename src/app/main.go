737ec7de5d2a26c891ef54ea9fcb08f4b8410ddc
luis-mtzz <75559152+luis-mtzz@users.noreply.github.com>
2024-07-10 21:55:03 -0500

    moved route to routes dir and simplified main.go

--- src/app/main.go
+++ src/app/main.go
@@ -1,66 +1,15 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"net/http"
-
 	"github.com/gin-gonic/gin"
-	"github.com/luis-mtzz/app/models"
+	"github.com/luis-mtzz/app/routes"
 )
 
 func main() {
 	r := gin.Default()
 
 	// Grabs all releases in a users collection
-	r.GET("/:user/releases", func(c *gin.Context) {
-		user := c.Param("user")
-		apiURL := fmt.Sprintf("https://api.discogs.com/users/%s/collection/folders/0/releases?sort=artist", user)
-		response, err := http.Get(apiURL)
-		if err != nil {
-			c.Status(http.StatusServiceUnavailable)
-			return
-		}
-		defer response.Body.Close()
-
-		if response.StatusCode != http.StatusOK {
-			c.Status(http.StatusServiceUnavailable)
-			return
-		}
-
-		var result models.CollectionItems
-		if err := json.NewDecoder(response.Body).Decode(&result); err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{
-				"error": "can not decode JSON",
-			})
-			return
-		}
-
-		// Extract the desired fields from each release
-		var simplifiedReleases []models.Release
-		for _, release := range result.Releases {
-			simplifiedRelease := models.Release{
-				ArtistName: release.BasicInformation.Artists[0].Name,
-				Genres:     release.BasicInformation.Genres,
-				Title:      release.BasicInformation.Title,
-			}
-			simplifiedReleases = append(simplifiedReleases, simplifiedRelease)
-		}
-
-		// Print the simplified releases
-		for _, sr := range simplifiedReleases {
-			fmt.Printf("Artist: %s, Title: %s, Genres: %v\n", sr.ArtistName, sr.Title, sr.Genres)
-		}
-
-		// Convert the simplified releases to JSON
-		formattedJSON, err := json.MarshalIndent(simplifiedReleases, "", "  ")
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "can't format JSON"})
-			return
-		}
-
-		c.Data(http.StatusOK, "application/json", formattedJSON)
-	})
+	r.GET("/:user/releases", routes.GetCollection)
 
 	r.Run()
 }
